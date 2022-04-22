37a1f1a9235a01b293d7ceb32bdb28ae68506795
Jeremy Nimmer <jeremy.nimmer@tri.global>
2022-04-22 07:47:14 -0700

    rewrite service_test for compactness and clarity

--- geometry/render_gltf_client/test/internal_http_service_test.cc
+++ geometry/render_gltf_client/test/internal_http_service_test.cc
@@ -2,7 +2,7 @@
 
 #include <fstream>
 
-#include <fmt/format.h>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "drake/common/filesystem.h"
@@ -17,23 +17,20 @@ namespace internal {
 namespace {
 
 namespace fs = drake::filesystem;
-using data_map_t = std::map<std::string, std::string>;
-using file_name_path_t = std::pair<std::string, std::optional<std::string>>;
-using file_map_t = std::map<std::string, file_name_path_t>;
+using FileFieldPayload = std::pair<std::string, std::optional<std::string>>;
+using FileFieldsMap = std::map<std::string, FileFieldPayload>;
 
 // A concrete implementation of HttpService that does nothing.
 class EmptyService : public HttpService {
  public:
-  EmptyService() : HttpService() {}
+  EmptyService() = default;
 
  protected:
   HttpResponse DoPostForm(
-      const std::string& /* temp_directory */, const std::string& url,
+      const std::string& /* temp_directory */, const std::string& /* url */,
       int /* port */,
       const std::map<std::string, std::string>& /* data_fields */,
-      const std::map<std::string,
-                     std::pair<std::string, std::optional<std::string>>>&
-          file_fields,
+      const FileFieldsMap& /* file_fields */,
       bool /* verbose */ = false) override {
     HttpResponse ret;
     ret.http_code = 200;
@@ -41,108 +38,74 @@ class EmptyService : public HttpService {
   }
 };
 
-GTEST_TEST(HttpService, ThrowIfFilesMissing) {
-  // Create an EmptyService and some files to test with.
-  EmptyService es;
-  const auto temp_dir = drake::temp_directory();
-  const std::string url{"127.0.0.1/render"};
-  const int port{8000};
-  const data_map_t empty_data_fields;
+class HttpServicePostFormTest : public ::testing::Test {
+ public:
+  HttpServicePostFormTest() {
+    // Create temporary files for this test case.
+    std::ofstream txt_file{txt_path_};
+    txt_file << "test!\n";
+    txt_file.close();
 
-  const auto test_txt_path = (fs::path(temp_dir) / "test.txt").string();
-  std::ofstream test_txt{test_txt_path};
-  test_txt << "test!\n";
-  test_txt.close();
-
-  const auto fake_jpg_path = (fs::path(temp_dir) / "fake.jpg").string();
-  std::ofstream fake_jpg{fake_jpg_path};
-  fake_jpg << "not really a jpg!\n";
-  fake_jpg.close();
-
-  {
-    file_map_t file_fields;
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {"test", {test_txt_path, std::nullopt}}));
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {"image", {fake_jpg_path, "image/jpeg"}}));
-    // No exception should be thrown if all files are present.
-    DRAKE_EXPECT_NO_THROW(
-        es.PostForm(temp_dir, url, port, empty_data_fields, file_fields));
+    std::ofstream jpg_file{jpg_path_};
+    jpg_file << "not really a jpg!\n";
+    jpg_file.close();
   }
 
-  // Some file paths that do not exist for testing.
-  const std::string missing_1_key = "missing_1";
-  const std::pair<std::string, std::optional<std::string>> missing_1_value = {
-      "/unlikely/to/exist.file_extension", std::nullopt};
-  const std::string missing_1_desc{
-      "missing_1='/unlikely/to/exist.file_extension'"};
-  ASSERT_FALSE(fs::is_regular_file(missing_1_value.first));
-
-  const std::string missing_2_key = "missing_2";
-  const std::pair<std::string, std::optional<std::string>> missing_2_value = {
-      "/this/is/not/a.real_file", std::nullopt};
-  const std::string missing_2_desc{"missing_2='/this/is/not/a.real_file'"};
-  ASSERT_FALSE(fs::is_regular_file(missing_2_value.first));
-
-  // The exception message prefix.
-  const std::string prefix = "Provided file fields had missing file\\(s\\): ";
-  // Since it is a map, order can change -- only support building regex for 2.
-  auto make_regex = [&prefix](const std::string& p1, const std::string& p2) {
-    return fmt::format("{0}(({1}, {2})|({2}, {1}))\\.", prefix, p1, p2);
-  };
-
-  {
-    file_map_t file_fields;
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {missing_1_key, missing_1_value}));
-    // Exception thrown: one file, does not exist.
-    DRAKE_EXPECT_THROWS_MESSAGE(
-        es.PostForm(temp_dir, url, port, empty_data_fields, file_fields),
-        prefix + missing_1_desc + "\\.");
+  // Exercise HttpService::PostForm using the given file_fields mapping from
+  // field name to file path.  (The mime_type is always nullopt.)
+  void PostForm(const std::map<std::string, std::string>& file_fields) {
+    FileFieldsMap actual_fields;
+    for (const auto& [field_name, file_path] : file_fields) {
+      FileFieldPayload payload;
+      payload.first = file_path;
+      actual_fields.emplace(field_name, std::move(payload));
+    }
+    EmptyService empty_service;
+    const std::string url = "http://127.0.0.1/render";
+    const int port = 8000;
+    const std::map<std::string, std::string> string_fields;
+    empty_service.PostForm(temp_dir_, url, port, string_fields, actual_fields);
   }
 
-  {
-    file_map_t file_fields;
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {missing_1_key, missing_1_value}));
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {missing_2_key, missing_2_value}));
-    // Exception thrown: multiple files, none exist.
-    DRAKE_EXPECT_THROWS_MESSAGE(
-        es.PostForm(temp_dir, url, port, empty_data_fields, file_fields),
-        make_regex(missing_1_desc, missing_2_desc));
-  }
+ protected:
+  const std::string temp_dir_{drake::temp_directory()};
+  const std::string txt_path_{temp_dir_ + "/test.txt"};
+  const std::string jpg_path_{temp_dir_ + "/fake.jpg"};
+};
 
-  {
-    file_map_t file_fields;
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {"test", {test_txt_path, std::nullopt}}));
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {missing_1_key, missing_1_value}));
-    // Exception thrown: one file exists, the other does not.
-    DRAKE_EXPECT_THROWS_MESSAGE(
-        es.PostForm(temp_dir, url, port, empty_data_fields, file_fields),
-        prefix + missing_1_desc + "\\.");
-  }
+// When no files are posted, no exceptions appear.
+TEST_F(HttpServicePostFormTest, NoFiles) {
+  DRAKE_EXPECT_NO_THROW(PostForm({}));
+}
 
-  {
-    file_map_t file_fields;
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {"test", {test_txt_path, std::nullopt}}));
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {missing_1_key, missing_1_value}));
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {"image", {fake_jpg_path, "image/jpeg"}}));
-    file_fields.insert(std::pair<std::string, file_name_path_t>(
-        {missing_2_key, missing_2_value}));
-    // Exception thrown: multiple files exist, multiple do not.
-    DRAKE_EXPECT_THROWS_MESSAGE(
-        es.PostForm(temp_dir, url, port, empty_data_fields, file_fields),
-        make_regex(missing_1_desc, missing_2_desc));
-  }
+// When valid filenames are provided, no exceptions appear.
+TEST_F(HttpServicePostFormTest, ValidFiles) {
+  DRAKE_EXPECT_NO_THROW(PostForm({{"text", txt_path_}}));
+  DRAKE_EXPECT_NO_THROW(PostForm({{"image", jpg_path_}}));
+  DRAKE_EXPECT_NO_THROW(PostForm({{"text", txt_path_}, {"image", jpg_path_}}));
+}
 
-  // 3 deletions: 2 files + 1 folder.
-  EXPECT_EQ(fs::remove_all(temp_dir), 3);
+// A request to upload a missing file will throw.
+TEST_F(HttpServicePostFormTest, NoSuchFile) {
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      PostForm({{"foo", "/no/such/file"}}),
+      ".*missing.*foo.*/no/such/file.*");
+}
+
+// A request to upload a multiple missing files reports all of the errors.
+TEST_F(HttpServicePostFormTest, NoSuchFileMultiple) {
+  DRAKE_EXPECT_THROWS_MESSAGE(
+      PostForm({{"bar", "/no/such/other"}, {"foo", "/no/such/file"}}),
+      ".*missing.*bar.*/no/such/other.*foo.*/no/such/file.*");
+}
+
+// When one files exists and one does not, only the missing file is reported.
+TEST_F(HttpServicePostFormTest, MixedFiles) {
+  EXPECT_THAT(
+      [&]() { PostForm({{"image", jpg_path_}, {"foo", "/no/such/file"}}); },
+      testing::ThrowsMessage<std::exception>(testing::AllOf(
+          testing::Not(testing::HasSubstr("image")),
+          testing::HasSubstr("/no/such/file"))));
 }
 
 }  // namespace
