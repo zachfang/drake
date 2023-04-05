660868f7a574d70c213bc56d39015d5207d72a34
Zach Fang <zach.fang@tri.global>
2023-04-05 12:04:50 -0400

    [geometry] use vtkOBJImporter to load .mtl file in RenderEngineVtk

--- geometry/render_gltf_client/test/client_demo.cc
+++ geometry/render_gltf_client/test/client_demo.cc
@@ -74,7 +74,7 @@ using drake::geometry::RenderEngineGltfClientParams;
 
 // TODO(jwnimmer-tri) This is way too much configuration data to parse using
 // gflags. Rewrite to use YAML input, instead.
-DEFINE_double(simulation_time, 10.0,
+DEFINE_double(simulation_time, 1.0,
               "Desired duration of the simulation in seconds.");
 DEFINE_bool(color, true, "Sets the enabled camera to render color");
 DEFINE_bool(depth, true, "Sets the enabled camera to render depth");
@@ -91,7 +91,7 @@ DEFINE_double(render_fps, 10, "Frames per simulation second to render");
    Diffuse: "0.0, 0.0, 0.0, 1.57, 3.14, 0.0"
    Textured: "0.0, 0.0, 0.0, -1.57, 0.0, 0.0"
  */
-DEFINE_string(camera_xyz_rpy, "0.8, 0.0, 0.5, -2.2, 0.0, 1.57",
+DEFINE_string(camera_xyz_rpy, "5.0, 0.0, 0.0, -1.57, 0.0, 1.57",
               "Sets the camera pose by xyz (meters) and rpy (radians) values.");
 DEFINE_string(
     save_dir, "",
@@ -204,8 +204,9 @@ int DoMain() {
   // we don't want to have to wait for gravity to take effect to observe a
   // difference in position.
   Parser parser{&plant};
+  parser.SetAutoRenaming(true);
   parser.AddModels(FindResourceOrThrow(
-      "drake/geometry/render_gltf_client/test/example_scene.sdf"));
+      "drake/geometry/render/test/meshes/box.obj"));
 
   DrakeLcm lcm;
   DrakeVisualizerd::AddToBuilder(&builder, scene_graph, &lcm);
@@ -292,13 +293,14 @@ int DoMain() {
 
   systems::Simulator<double> simulator(*diagram);
 
-  auto& context = static_cast<systems::DiagramContext<double>&>(
-      simulator.get_mutable_context());
-  auto& plant_context = plant.GetMyMutableContextFromRoot(&context);
+  // auto& context = static_cast<systems::DiagramContext<double>&>(
+  //    simulator.get_mutable_context());
+  // auto& plant_context = plant.GetMyMutableContextFromRoot(&context);
 
   // Initialize the moving bottle's position and speed so we can observe motion.
   // The mustard bottle spins while climbing slightly.
   plant.mutable_gravity_field().set_gravity_vector({0, 0, 0});
+  /*
   const Body<double>& mustard_body = plant.GetBodyByName(
       "base_link_mustard",
       plant.GetModelInstanceByName("example_scene::mustard_bottle"));
@@ -308,7 +310,7 @@ int DoMain() {
   const SpatialVelocity<double> V_WMustardBottle(Vector3d{0.6, 0, 0},
                                                  Vector3d{0, 0, 0.1});
   plant.SetFreeBodySpatialVelocity(&plant_context, mustard_body,
-                                   V_WMustardBottle);
+                                   V_WMustardBottle);*/
   simulator.set_target_realtime_rate(1.f);
   simulator.AdvanceTo(FLAGS_simulation_time);
 
